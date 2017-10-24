2e64cbe543c968c9ecdea2ab508f4ab43919f6de
greenleafm19 <greenleafm19@mail.wlu.edu>
2017-10-24 17:29:11 -0400

    Added many things, such as WarGame and edited some MainViews

--- project 5/src/MainView.java
+++ project 5/src/MainView.java
@@ -10,21 +10,52 @@ import java.awt.event.*;
 public class MainView extends JFrame{
 
     private Deck deck;
+    private WarGame model;
 
     public MainView(Deck deck){
         this.deck = deck;
-        CardPanel panel = new CardPanel();
-        JButton button = new JButton("Deal");
-        button.addActionListener(new ActionListener(){
+        this.setTitle("The Game of War");
+        
+        JLabel player1Label = new JLabel("Player 1");
+        JLabel statusLabel = new JLabel("Game Status");
+        JLabel player2Label = new JLabel("Player 2");
+        
+        CardPanel panel1 = new CardPanel();
+        JTextField statusField = new JTextField(1);
+        statusField.setEditable(false);
+        statusField.setHorizontalAlignment(JTextField.CENTER);
+        statusField.setText(model.toString());
+        CardPanel panel2 = new CardPanel();
+        JButton moveButton = new JButton("Move");
+        JButton newButton = new JButton("New Game");
+        
+        moveButton.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
                 if (! deck.isEmpty()){
                     Card card = deck.deal();
                     card.turn();
-                    panel.setCard(card);
+                    panel1.setCard(card);
+                    panel2.setCard(card);
                 }
             }});
+        
+        newButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                if (! deck.isEmpty()){
+                    Card card = deck.deal();
+                    card.turn();
+                    panel1.setCard(card);
+                    panel2.setCard(card);
+                }
+            }});
+        
         Container c = getContentPane();
-        c.add(panel, BorderLayout.CENTER);
-        c.add(button, BorderLayout.SOUTH);
+        c.setLayout(new GridLayout(3, 3));
+        c.add(player1Label);
+        c.add(statusLabel);
+        c.add(player2Label);
+        c.add(panel1);
+        c.add(statusField);
+        c.add(panel2);
     }
 }
